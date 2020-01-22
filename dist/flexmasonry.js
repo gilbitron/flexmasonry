d45fcd84af1a8214af0f00cba069adcba2f1bc99
Joshua Crewe <joshua@venncreative.co.uk>
2020-01-22 11:26:47 +0000

    Check number of items vs number of columns
    If the number of items in the grid is less than the number of columns
    defined then add additional items

--- dist/flexmasonry.js
+++ dist/flexmasonry.js
@@ -4,4 +4,127 @@
  * Author: Gilbert Pellegrom <gilbert@pellegrom.me>
  * License: MIT
  */
-var FlexMasonry=function(e){var n={};function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}return t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:r})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,n){if(1&n&&(e=t(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(t.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var o in e)t.d(r,o,function(n){return e[n]}.bind(null,o));return r},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="",t(t.s=0)}([function(e,n,t){t(1),e.exports=t(2)},function(e,n,t){},function(e,n,t){"use strict";t.r(n);const r={responsive:!0,breakpointCols:{"min-width: 1500px":6,"min-width: 1200px":5,"min-width: 992px":4,"min-width: 768px":3,"min-width: 576px":2},numCols:4};let o=null,i={},s=[];function a(){s.forEach(function(e){c(e)})}function l(){o&&window.cancelAnimationFrame(o),o=window.requestAnimationFrame(function(){y()})}function c(e){if(d()<2)return void e.style.removeProperty("height");let n=[];Array.from(e.children).forEach(function(e){if(e.classList.contains("flexmasonry-break"))return;const t=window.getComputedStyle(e),r=t.getPropertyValue("order"),o=t.getPropertyValue("height");n[r-1]||(n[r-1]=0),n[r-1]+=Math.ceil(parseFloat(o))});const t=Math.max(...n);e.style.height=t+"px"}function f(e){const n=e.querySelectorAll(".flexmasonry-break");if(Array.from(n).length!==d()-1)for(let n=1;n<d();n++){const t=document.createElement("div");t.classList.add("flexmasonry-break"),t.classList.add("flexmasonry-break-"+n),e.appendChild(t)}}function u(e){e.classList.contains("flexmasonry-cols-"+d())||(e.className=e.className.replace(/(flexmasonry-cols-\d+)/,""),e.classList.add("flexmasonry-cols-"+d()))}function d(){if(!i.responsive)return i.numCols;const e=Object.keys(i.breakpointCols);for(const n of e)if(window.matchMedia("("+n+")").matches)return i.breakpointCols[n];return 1}function m(e,n={}){return i=Object.assign(r,n),u(e),function(e){const n=e.querySelectorAll(".flexmasonry-break");Array.from(n).length!==d()-1&&Array.from(n).forEach(function(e){e.parentNode.removeChild(e)})}(e),f(e),c(e),this}function y(e={}){return s.forEach(function(n){m(n,e)}),this}n.default={init:function(e,n={}){return s="string"==typeof e?document.querySelectorAll(e):e,i=Object.assign(r,n),s.forEach(function(e){!function(e){e.classList.add("flexmasonry"),i.responsive&&e.classList.add("flexmasonry-responsive"),u(e),Array.from(e.children).forEach(function(e){e.classList.add("flexmasonry-item")}),f(e)}(e),c(e)}),window.addEventListener("load",a),window.addEventListener("resize",l),this},refresh:m,refreshAll:y,destroyAll:function(){window.removeEventListener("load",a),window.removeEventListener("resize",l)}}}]).default;
+var FlexMasonry =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/flexmasonry.css":
+/*!*****************************!*\
+  !*** ./src/flexmasonry.css ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack://FlexMasonry/./src/flexmasonry.css?");
+
+/***/ }),
+
+/***/ "./src/flexmasonry.js":
+/*!****************************!*\
+  !*** ./src/flexmasonry.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nconst defaultOptions = {\n    /*\n     * If `responsive` is `true`, `breakpointCols` will be used to determine\n     * how many columns a grid should have at a given responsive breakpoint.\n     */\n    responsive: true,\n    /*\n     * A list of how many columns should be shown at different responsive\n     * breakpoints, defined by media queries.\n     */\n    breakpointCols: {\n        'min-width: 1500px': 6,\n        'min-width: 1200px': 5,\n        'min-width: 992px': 4,\n        'min-width: 768px': 3,\n        'min-width: 576px': 2,\n    },\n    /*\n     * If `responsive` is `false`, this number of columns will always be shown,\n     * no matter the width of the screen.\n     */\n    numCols: 4,\n};\n\nlet _resizeId = null;\nlet _options = {};\nlet _targets = [];\n\nfunction init(targets, options = {}) {\n    if (typeof targets === 'string') {\n        _targets = document.querySelectorAll(targets);\n    } else {\n        _targets = targets;\n    }\n\n    _options = Object.assign(defaultOptions, options);\n\n    _targets.forEach(function(target) {\n        setUp(target);\n        setHeight(target);\n    });\n\n    addEventListeners();\n\n    return this;\n}\n\nfunction setUp(target) {\n    target.classList.add('flexmasonry');\n\n    if (_options.responsive) {\n        target.classList.add('flexmasonry-responsive');\n    }\n\n    setColsClass(target);\n\n    Array.from(target.children).forEach(function(item) {\n        item.classList.add('flexmasonry-item');\n    });\n\n    // Check if we have fewer items than columns\n    if(target.children.length < getCurrentCols()) {\n        // Work out how many more items we would need to fill the grid\n        const itemDeficit = getCurrentCols() - target.children.length;\n        // Create an empty item ...\n        const el = document.createElement('div');\n        // ... add the right class.\n        el.classList.add('flexmasonry-item');\n        // For each new item in the grid we need\n        for (var i = 0, len = itemDeficit; i < len; i++) {\n            // Append it to the grid\n            target.appendChild(el.cloneNode(true));\n        }\n     }\n\n    addBreakElements(target);\n}\n\nfunction onLoad() {\n    _targets.forEach(function(target) {\n        setHeight(target);\n    });\n}\n\nfunction onResize() {\n    if (_resizeId) {\n        window.cancelAnimationFrame(_resizeId);\n    }\n\n    _resizeId = window.requestAnimationFrame(function() {\n        refreshAll();\n    });\n}\n\nfunction addEventListeners() {\n    window.addEventListener('load', onLoad);\n    window.addEventListener('resize', onResize);\n}\n\nfunction removeEventListeners() {\n    window.removeEventListener('load', onLoad);\n    window.removeEventListener('resize', onResize);\n}\n\nfunction setHeight(target) {\n    if (getCurrentCols() < 2) {\n        target.style.removeProperty('height');\n        return;\n    }\n\n    let heights = [];\n\n    Array.from(target.children).forEach(function(item) {\n        if (item.classList.contains('flexmasonry-break')) {\n            return;\n        }\n\n        const comp   = window.getComputedStyle(item);\n        const order  = comp.getPropertyValue('order');\n        const height = comp.getPropertyValue('height');\n\n        if (!heights[order - 1]) {\n            heights[order - 1] = 0;\n        }\n        heights[order - 1] += Math.ceil(parseFloat(height));\n    });\n\n    const maxHeight = Math.max(...heights);\n    target.style.height = maxHeight + 'px';\n}\n\nfunction addBreakElements(target) {\n    const breakEls = target.querySelectorAll('.flexmasonry-break');\n    if (Array.from(breakEls).length === (getCurrentCols() - 1)) {\n        return;\n    }\n\n    for (let i = 1; i < getCurrentCols(); i++) {\n        const breakDiv = document.createElement('div');\n        breakDiv.classList.add('flexmasonry-break');\n        breakDiv.classList.add('flexmasonry-break-' + i);\n        target.appendChild(breakDiv);\n    }\n}\n\nfunction removeBreakElements(target) {\n    const breakEls = target.querySelectorAll('.flexmasonry-break');\n    if (Array.from(breakEls).length === (getCurrentCols() - 1)) {\n        return;\n    }\n\n    Array.from(breakEls).forEach(function(breakEl) {\n        breakEl.parentNode.removeChild(breakEl);\n    });\n}\n\nfunction setColsClass(target) {\n    if (target.classList.contains('flexmasonry-cols-' + getCurrentCols())) {\n        return;\n    }\n\n    target.className = target.className.replace(/(flexmasonry-cols-\\d+)/, '');\n    target.classList.add('flexmasonry-cols-' + getCurrentCols());\n}\n\nfunction getCurrentCols() {\n    if (!_options.responsive) {\n        return _options.numCols;\n    }\n\n    const keys = Object.keys(_options.breakpointCols);\n    for (const key of keys) {\n        if (window.matchMedia('(' + key + ')').matches) {\n            return _options.breakpointCols[key];\n        }\n    }\n\n    return 1;\n}\n\nfunction refresh(target, options = {}) {\n    _options = Object.assign(defaultOptions, options);\n\n    setColsClass(target);\n    removeBreakElements(target);\n    addBreakElements(target);\n    setHeight(target);\n\n    return this;\n}\n\nfunction refreshAll(options = {}) {\n    _targets.forEach(function(target) {\n        refresh(target, options);\n    });\n\n    return this;\n}\n\nfunction destroyAll() {\n    removeEventListeners();\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n    init,\n    refresh,\n    refreshAll,\n    destroyAll,\n});\n\n\n//# sourceURL=webpack://FlexMasonry/./src/flexmasonry.js?");
+
+/***/ }),
+
+/***/ 0:
+/*!********************************************************!*\
+  !*** multi ./src/flexmasonry.css ./src/flexmasonry.js ***!
+  \********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! ./src/flexmasonry.css */\"./src/flexmasonry.css\");\nmodule.exports = __webpack_require__(/*! ./src/flexmasonry.js */\"./src/flexmasonry.js\");\n\n\n//# sourceURL=webpack://FlexMasonry/multi_./src/flexmasonry.css_./src/flexmasonry.js?");
+
+/***/ })
+
+/******/ })["default"];
